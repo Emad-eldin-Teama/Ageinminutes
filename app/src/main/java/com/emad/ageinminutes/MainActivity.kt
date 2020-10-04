5d6ba6f08c7887b9b8d7793b74671f3b031f0bcf
emad el din Emadov <emadov01@gmail.com>
2020-10-04 07:16:49 +0200

    Initial commit

---
+++ app/src/main/java/com/emad/ageinminutes/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.emad.ageinminutes
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnDatePicker.setOnClickListener{view ->
+            clickDatePicker(view)
+
+        }
+
+    }
+    fun clickDatePicker(view: View){
+
+        val myCalender = Calendar.getInstance()
+        val year= myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+       val dpd=DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+           val selectedDate= "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+           tvSelectedDate.text= selectedDate.toString()
+           val sdf = SimpleDateFormat("dd/MM/yyyy",Locale.ENGLISH)
+           val theDate= sdf.parse(selectedDate)
+           val selectedDateInMinutes=theDate!!.time /60000
+           val currentDate= sdf.parse(sdf.format(System.currentTimeMillis()))
+           val currentDateToMinutes = currentDate!!.time/60000
+           val differenceInMinutes = currentDateToMinutes- selectedDateInMinutes
+           tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+
+       },year,month,day)
+        dpd.datePicker.setMaxDate(Date().time -8640000)
+        dpd.show()
+    }
+
+}
